484655749d8280e7292ef21d21f7ae211a2f064e
Roy Quilor <royquilor@gmail.com>
2018-10-19 13:42:31 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -7,25 +7,29 @@ const pages = [
     isrc: "images/testimonials/theo.jpg",
     copy: "Roy is a very nice, very accomplished UX/UI designer who delivers spot on work. In my time working with him, our company saw him as a key contributor to the development of our products, and as a high value, trusted member of the client services team. He has an enviable eye for detail and the ability to really focus on the end user at every stage along a journey: leading to truly valuable work.",
     name: "Theo Bonham Carter",
-    title: "Lucidworks"
+    title: "Lucidworks",
+    number: "1"
   },
   {
     isrc: "images/testimonials/andy.jpg",
     copy: "Roy is an awesome designer who loves simplicity, typography and useful products. Having worked alongside Roy for 3 years I know that he's really dedicated to his work and a great guy to have on your team. I'd fully recommend Roy for any web and mobile app design projects!",
     name: "Andy Shaw",
-    title: "Mobext"
+    title: "Mobext",
+    number: "2"
   },
   {
     isrc: "images/testimonials/rob.jpg",
     copy: "Roy created our website from start to finish and did a fantastic job on the creation, styling and usability as well as keeping it orientated around user experience and ultimately conversion goals. Would happily work with Roy again and wouldn't hesitate to recommend him",
     name: "Robert Jones",
-    title: "Property Investments UK"
+    title: "Property Investments UK",
+    number: "3"
   },
   {
     isrc: "images/testimonials/russ.jpg",
     copy: "Roy is a good designer who can prototype his own designs and can iterate quickly when working to a tight deadline. He worked well with developers, designers, project managers and clients. A pleasure and honour to work with for 5 years.",
     name: "Russell Barr",
-    title: "Bynder"
+    title: "Bynder",
+    number: "4"
   },
   {
     isrc:
@@ -35,7 +39,8 @@ const pages = [
     name:
     "Matthew Griffiths",
     title:
-    "Bynder"
+    "Bynder",
+    number: "5"
   },
   {
     isrc:
@@ -45,7 +50,8 @@ const pages = [
     name:
     "Toby O'Brien",
     title:
-    "Chatham House"
+    "Chatham House",
+    number: "6"
   },
   {
     isrc:
@@ -55,16 +61,18 @@ const pages = [
     name:
     "Landry Karege",
     title:
-    "GlobalData Plc"
+    "GlobalData Plc",
+    number: "7"
   }
 ]
 
 const nextTag = document.querySelector(".next")
-const previousTag = document.querySelector(".prev")
-const outputTag = document.querySelector(".quote h4")
-const nameTag = document.querySelector(".quote-wrap p")
+// const previousTag = document.querySelector(".prev")
+const outputTag = document.querySelector(".quote blockquote")
+const nameTag = document.querySelector(".quote-wrap cite")
 const titleTag = document.querySelector(".quote span")
 const imgSrc = document.querySelector(".quote-wrap img")
+const numTag = document.querySelector(".number span")
 
 const next = function() {
   pageNumber = pageNumber + 1
@@ -86,6 +94,7 @@ const updateSection = function() {
   outputTag.innerHTML = pages[pageNumber].copy
   nameTag.innerHTML = pages[pageNumber].name
   titleTag.innerHTML = pages[pageNumber].title
+  numTag.innerHTML = pages[pageNumber].number
   imgSrc.src = pages[pageNumber].isrc
 }
 
@@ -96,15 +105,15 @@ nextTag.addEventListener("click", function() {
   outputTag.classList.add("fadeInLeft")
 })
 
-previousTag.addEventListener("click", function() {
-  previous()
-})
+// previousTag.addEventListener("click", function() {
+//   previous()
+// })
 
 document.addEventListener("keyup", function () {
   if(event.key == "ArrowRight") {
     next()
   }
-  if(event.key == "ArrowLeft") {
-    previous()
-  }
+  // if(event.key == "ArrowLeft") {
+  //   previous()
+  // }
 })
--- js/tweets.js
+++ js/tweets.js
@@ -2,11 +2,12 @@ let tweetNumber = 0
 
 const tweets = [
   {
-    isrc: "images/tweets/cham.jpg",
+    isrc: "images/tweets/snaps.jpg",
     copy:
-    "Awesome!! Thank you so much for creating the web app to save us all a ton of time trying to find the best IVs for Eevee. @pokecuno 🙌",
-    name: "@SamuraiBirdee",
-    link: "https://twitter.com/SamuraiBirdee/status/1027967692429451264",
+    "iOS users who want to check IVs quickly should take a look at this video. The website I used to copy the string is http://hundoiv.com  so you should check it out! Feel free to share! Note: Always appraise as CP can be the same for different levels. #PokemonGO",
+    name: "@pkmnsnaps",
+    link: "https://twitter.com/pkmnsnaps/status/1027761510494265344",
+    num: "1"
   },
   {
     isrc: "images/tweets/collective-md.jpg",
@@ -14,12 +15,15 @@ const tweets = [
     "If you are looking for a quick way to IV check for tomorrow's event. This is a great help. Found me 2 perfect Larvitars and the lower strings found me a 98 Squirtle.",
     name: "@collective_md",
     link: "https://twitter.com/collective_md/status/1043216933817118726",
+    num: "2"
   },
   {
-    isrc: "images/tweets/snaps.jpg",
-    copy: "iOS users who want to check IVs quickly should take a look at this video. The website I used to copy the string is http://hundoiv.com  so you should check it out! Feel free to share! Note: Always appraise as CP can be the same for different levels. #PokemonGO",
-    name: "@pkmnsnaps",
-    link: "https://twitter.com/pkmnsnaps/status/1027761510494265344",
+    isrc: "images/tweets/cham.jpg",
+    copy:
+    "Awesome!! Thank you so much for creating the web app to save us all a ton of time trying to find the best IVs for Eevee. @pokecuno 🙌",
+    name: "@SamuraiBirdee",
+    link: "https://twitter.com/SamuraiBirdee/status/1027967692429451264",
+    num: "3"
   },
   {
     isrc: "images/tweets/cmd.jpg",
@@ -27,14 +31,15 @@ const tweets = [
     "Works like a charm! 😍 Here are just a few jems found in the last few minutes of #CommunityDay using @pokecuno strings. Definitely added a few min to catch more Larvitar!🦖🦖🦖",
     name: "@TeamMysticCMD",
     link: "https://twitter.com/TeamMysticCMD/status/1012371783645913088",
+    num: "4"
   }
 ]
 
 const nextTag = document.querySelector(".tweets .next")
 const previousTag = document.querySelector(".tweets .prev")
-const outputTag = document.querySelector(".quote h4")
-const nameTag = document.querySelector(".quote-wrap p")
-const titleTag = document.querySelector(".quote span")
+const outputTag = document.querySelector(".quote blockquote")
+const nameTag = document.querySelector(".quote-wrap cite")
+const tweetNumTag = document.querySelector(".number span")
 const imgSrc = document.querySelector(".quote-wrap img")
 
 const next = function() {
@@ -56,7 +61,7 @@ const previous = function() {
 const updateTweet = function() {
   outputTag.innerHTML = tweets[tweetNumber].copy
   nameTag.innerHTML = tweets[tweetNumber].name
-  titleTag.innerHTML = tweets[tweetNumber].title
+  tweetNumTag.innerHTML = tweets[tweetNumber].num
   imgSrc.src = tweets[tweetNumber].isrc
 }
 
